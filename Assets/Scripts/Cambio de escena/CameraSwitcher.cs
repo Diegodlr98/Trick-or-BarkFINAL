42739bf3310a426a13134988bea439167130d49e
Agallardo710 <agallardo.a3d2n24@lamerce.com>
2025-04-30 00:21:29 +0200

    Nueva Pantalla de Inicio

--- Assets/Scripts/Cambio de escena/CameraSwitcher.cs
+++ Assets/Scripts/Cambio de escena/CameraSwitcher.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CameraSwitcher : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Camera cameraA;
+    public Camera cameraB;
+
+    private void Start()
     {
-        
+        int lastUsed = PlayerPrefs.GetInt("LastCameraUsed", 0);
+
+        if (lastUsed == 0)
+        {
+            ActivateCamera(cameraB, cameraA);
+            PlayerPrefs.SetInt("LastCameraUsed", 1);
+        }
+        else
+        {
+            ActivateCamera(cameraA, cameraB);
+            PlayerPrefs.SetInt("LastCameraUsed", 0);
+        }
+
+        PlayerPrefs.Save();
     }
 
-    // Update is called once per frame
-    void Update()
+    void ActivateCamera(Camera toActivate, Camera toDeactivate)
     {
-        
+        if (toActivate != null) toActivate.gameObject.SetActive(true);
+        if (toDeactivate != null) toDeactivate.gameObject.SetActive(false);
     }
 }
