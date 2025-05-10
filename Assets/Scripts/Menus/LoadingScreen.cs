74dc1a1b882600aa719be2b02e9ba94a20d09d60
Diegodlr98 <diego-delosrios@hotmail.com>
2025-05-10 08:26:36 +0200

    pantalla de carga falseada

--- Assets/Scripts/Menus/LoadingScreen.cs
+++ Assets/Scripts/Menus/LoadingScreen.cs
@@ -2,12 +2,13 @@ using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+
 public class LoadingScreen : MonoBehaviour
 {
+    [SerializeField] Image _LoadingBar;
 
-    [SerializeField]
-    Image _LoadingBar;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float minLoadTime = 2f; // Tiempo mínimo de pantalla de carga (en segundos)
+
     void Start()
     {
         StartCoroutine(LoadNextLevel());
@@ -15,11 +16,27 @@ public class LoadingScreen : MonoBehaviour
 
     IEnumerator LoadNextLevel()
     {
-        AsyncOperation loadLevel = SceneManager.LoadSceneAsync("Level1");
+        float fakeTimer = 0f;
 
-        while (loadLevel.isDone)
+        AsyncOperation loadLevel = SceneManager.LoadSceneAsync("Level1");
+        loadLevel.allowSceneActivation = false;
+
+        while (!loadLevel.isDone)
         {
-            _LoadingBar.fillAmount = Mathf.Clamp01(loadLevel.progress / .01f);
+            fakeTimer += Time.deltaTime;
+
+            // El progreso de Unity llega hasta 0.9 (el 0.1 restante es activación de escena)
+            float progress = Mathf.Clamp01(loadLevel.progress / 0.9f);
+            _LoadingBar.fillAmount = Mathf.Clamp01((progress + (fakeTimer / minLoadTime)) / 2f);
+
+            // Espera artificial hasta que pasen los segundos deseados y la escena esté lista
+            if (loadLevel.progress >= 0.9f && fakeTimer >= minLoadTime)
+            {
+                _LoadingBar.fillAmount = 1f;
+                yield return new WaitForSeconds(0.5f); // pequeña pausa al completar
+                loadLevel.allowSceneActivation = true;
+            }
+
             yield return null;
         }
     }
--- Assets/Scripts/Menus/LoadingScreen1.cs
+++ Assets/Scripts/Menus/LoadingScreen1.cs
@@ -1,13 +1,15 @@
+
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+
 public class LoadingScreen1 : MonoBehaviour
 {
+    [SerializeField] Image _LoadingBar;
 
-    [SerializeField]
-    Image _LoadingBarMenu;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float minLoadTime = 2f; // Tiempo mínimo de pantalla de carga (en segundos)
+
     void Start()
     {
         StartCoroutine(LoadNextMenu());
@@ -15,11 +17,27 @@ public class LoadingScreen1 : MonoBehaviour
 
     IEnumerator LoadNextMenu()
     {
-        AsyncOperation loadLevel = SceneManager.LoadSceneAsync("MainMenuFinal");
+        float fakeTimer = 0f;
 
-        while (loadLevel.isDone)
+        AsyncOperation loadLevel = SceneManager.LoadSceneAsync("MainMenuFinal");
+        loadLevel.allowSceneActivation = false;
+
+        while (!loadLevel.isDone)
         {
-            _LoadingBarMenu.fillAmount = Mathf.Clamp01(loadLevel.progress / .01f);
+            fakeTimer += Time.deltaTime;
+
+            // El progreso de Unity llega hasta 0.9 (el 0.1 restante es activación de escena)
+            float progress = Mathf.Clamp01(loadLevel.progress / 0.9f);
+            _LoadingBar.fillAmount = Mathf.Clamp01((progress + (fakeTimer / minLoadTime)) / 2f);
+
+            // Espera artificial hasta que pasen los segundos deseados y la escena esté lista
+            if (loadLevel.progress >= 0.9f && fakeTimer >= minLoadTime)
+            {
+                _LoadingBar.fillAmount = 1f;
+                yield return new WaitForSeconds(0.5f); // pequeña pausa al completar
+                loadLevel.allowSceneActivation = true;
+            }
+
             yield return null;
         }
     }
